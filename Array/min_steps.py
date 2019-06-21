ff245ecbc15493ba9a83d10b92567c01102224a5
Prem0707 <premprakash0796@gmail.com>
2019-06-22 00:55:56 +0530

    min steps in an infinite grid

---
+++ Array/min_steps.py
@@ -0,0 +1,42 @@
+"""
+	You are in an infinite 2D grid where you can move in any of the 8 directions :
+
+	 (x,y) to 
+	    (x+1, y), 
+	    (x - 1, y), 
+	    (x, y+1), 
+	    (x, y-1), 
+	    (x-1, y-1), 
+	    (x+1,y+1), 
+	    (x-1,y+1), 
+	    (x+1,y-1) 
+	You are given a sequence of points and the order in which you need to cover the points. Give the minimum number of steps in which you can achieve it. You start from the first point.
+
+	Input :
+
+	Given two integer arrays A and B, where A[i] is x coordinate and B[i] is y coordinate of ith point respectively.
+	Output :
+
+	Return an Integer, i.e minimum number of steps.
+	Example :
+
+	Input : [(0, 0), (1, 1), (1, 2)]
+	Output : 2
+	It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+
+"""
+
+
+
+class Solution:
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return an integer
+    def coverPoints(self, A, B):
+        no_of_steps = 0
+        for i in range(len(A)-1):
+            fir_point = (A[i], B[i])
+            sec_point = (A[i+1], B[i+1])
+            distance = max(abs(fir_point[0] - sec_point[0]), abs(fir_point[1] - sec_point[1]))
+            no_of_steps += distance
+        return no_of_steps
